cce6fd41f45111b7f74512ee51c13d630d3f0eb6
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-30 07:50:12 +0500

    finished

---
+++ js/index.js
@@ -0,0 +1,72 @@
+// 1 misol
+
+// let n = +prompt('son krting')
+// for( let i = 1 ; i <=n ; i++){
+//     if(i % 2 == 1){
+//         console.log(i);
+
+//     }
+// }
+
+// 2 misol
+
+// let n = + prompt('son krting')
+// for(let i = 1; i <=10; i++){
+//  console.log(i**3);
+// }
+
+// 3 misol
+// let n = +prompt('son krting');
+// for(let i = 1; i<=20; i++){
+//     let a = 1;
+
+// for(let n =1; n<=i; n++){
+
+// a=a*n
+
+// }
+// console.log(a);
+
+// }
+
+// 4 misol
+//  let a = 0;
+// let n = + prompt('son krting')
+// for(let i = 1; i <=30; i++){
+// a=a+i**2;
+//  console.log(a);
+
+// }
+
+// 5 misol
+
+// let n = +prompt("son krting");
+// for (let i = 1; i <= 20; i++) {
+//   let counter = 0;
+//   for (let a = 1; a <= i; a++) {
+//     if (i % a == 0) {
+//       counter++;
+//     }
+//   }
+
+//   if (counter == 2) {
+//     console.log(1);
+//   }
+// }
+
+// 6 misol
+// for (let i = 1; i<=100; i++){
+//   if(i*2<100){
+//     console.log(i);
+// }
+// }
+
+// 7 misol
+// for(let i=1; i<=100; i++){
+//  if(i%7==0 && i%5!=0){
+// console.log(i);
+// }
+// }
+
+// 8 miso
+
